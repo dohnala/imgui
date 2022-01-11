3223a2a6b83c2d382b1eda14e354a7144652275e
dohnal <dohnal.adam@gmail.com>
2022-01-11 22:19:01 +0100

    Add premake5.lua with support for glfw and opengl3

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+        "imgui_tables.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+	}
+
+    includedirs
+    {
+        "include",
+        "imgui_impl_opengl3.h",
+        "imgui_impl_opengl3.cpp",
+        "imgui_impl_glfw.h",
+        "imgui_impl_glfw.cpp",
+        "imgui_impl_opengl3_loader.h"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
